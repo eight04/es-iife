49ed2704ff6f1514c68beeb2ecdaf75da1df6b9d
eight04 <eight04@gmail.com>
2026-04-01 23:59:46 +0800

    Add: more test

--- test/cases/export-named-reassign/input.js
+++ test/cases/export-named-reassign/input.js
@@ -1,5 +1,14 @@
 export let foo = "123";
-setTimeout(() => {
+setTimeout(function*(){
   foo = "456";
+  [foo] = ["789"];
+  yield bar = ({foo} = {foo: "000"});
+  baz = () => ({foo} = {foo: "000"});
+  for (foo of ["111", "222"]) {
+    console.log(foo);
+  }
+  for (foo = 0; foo < 2; foo++) {
+    console.log(foo);
+  }
   console.log(foo);
 }, 1000);
